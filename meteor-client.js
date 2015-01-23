c231e78e04212c019aaee1910ab65b9e988d0aaa
Sergio Garcia Murillo <sergio.garcia.murillo@gmail.com>
2015-01-23 10:58:19 +0100

    First working version

---
+++ meteor-client.js
@@ -0,0 +1,20 @@
+module.exports = function(jQuery,underscore) {
+  var Meteor = require("meteor-core")(underscore);
+  require("meteor-base64")(Meteor);
+  require("meteor-ejson-safe")(Meteor);
+  require("meteor-random-window-crypto")(Meteor);
+  require("meteor-id-map")(Meteor);
+  require("meteor-ordered-dict")(Meteor);
+  require("meteor-minimongo")(Meteor);
+  require("meteor-tracker")(Meteor);
+  require("meteor-observe-sequence")(Meteor);
+  require("meteor-htmljs")(Meteor);
+  require("meteor-html-tools")(Meteor);
+  require("meteor-reactive-var")(Meteor);
+  require("meteor-blaze")(Meteor,jQuery);
+  require("meteor-blaze-tools")(Meteor);
+  require("meteor-templating")(Meteor);
+  require("meteor-spacebars")(Meteor);
+  require("meteor-spacebars-compiler")(Meteor);
+  return Meteor;
+};
